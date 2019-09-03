3c52553ffe38cc950f660ff0d3f3b6a76685a743
k-dominik <k-dominik@users.noreply.github.com>
2019-09-03 17:14:07 +0200

    allow evironment list files
    allow also files that were produced with conda list and conda create

--- src/conda_diff/cli.py
+++ src/conda_diff/cli.py
@@ -1,12 +1,12 @@
 import json
 from argparse import ArgumentParser
 
-from typing import Mapping, Union
-
+import pathlib
 import sh
-
 from conda_diff.env import CondaEnvironment, conda_environment_diff
 from conda_diff.pkg import Package
+from conda_diff.reader import read_env_json_file
+from typing import Iterable, Mapping, Union
 
 
 class CondaNotFound(Exception):
@@ -16,14 +16,18 @@ class CondaNotFound(Exception):
 def parse_args():
     p = ArgumentParser(description="conda-diff is a tool to compare two conda environments")
 
-    p.add_argument("environment_a", type=str, help="Environment name for environment a")
-    p.add_argument("environment_b", type=str, help="Environment name for environment b")
+    p.add_argument(
+        "environment_a", type=str, help="Environment name or path to environment json file for environment a"
+    )
+    p.add_argument(
+        "environment_b", type=str, help="Environment name or path to environment json file for environment b"
+    )
 
     args = p.parse_args()
     return args
 
 
-def get_conda_list(environment_name: str) -> Mapping[str, Union[int, str]]:
+def get_conda_list(environment_name: str) -> Iterable[Mapping[str, Union[int, str]]]:
     try:
         specs = sh.conda("list", "--name", environment_name, "--json")
     except sh.CommandNotFound:
@@ -38,11 +42,16 @@ def get_conda_list(environment_name: str) -> Mapping[str, Union[int, str]]:
 def main():
     args = parse_args()
 
-    list_a = get_conda_list(args.environment_a)
-    list_b = get_conda_list(args.environment_b)
+    lists = []
+    for env in [args.environment_a, args.environment_b]:
+        if pathlib.Path(env).exists():
+            env_list = read_env_json_file(pathlib.Path(env))
+            lists.append(env_list)
+        else:
+            lists.append(get_conda_list(env))
 
-    environment_a = CondaEnvironment(args.environment_a, list_a)
-    environment_b = CondaEnvironment(args.environment_b, list_b)
+    environment_a = CondaEnvironment(args.environment_a, lists[0])
+    environment_b = CondaEnvironment(args.environment_b, lists[1])
 
     diff = conda_environment_diff(environment_a, environment_b)
     print(diff)
--- src/conda_diff/reader.py
+++ src/conda_diff/reader.py
@@ -3,6 +3,8 @@ from pathlib import Path
 import json
 from typing import Iterable, Mapping, Union
 
+from conda_diff.pkg import Package
+
 
 def read_env_json_file(env_list_path: Path) -> Iterable[Mapping[str, Union[str, int]]]:
     with open(env_list_path, "r") as f:
@@ -10,4 +12,4 @@ def read_env_json_file(env_list_path: Path) -> Iterable[Mapping[str, Union[str, 
         if isinstance(data, dict):
             data = data["actions"]["LINK"]
 
-        return data
+        return [Package(**x) for x in data]
