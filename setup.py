f5c7f7947763e7c9b36208108e54e43c056907e5
k-dominik <k-dominik@users.noreply.github.com>
2024-07-31 18:08:44 +0200

    Bump version to 0.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="conda-diff",
-    version="0.2.0dev1",
+    version="0.3.0",
     author="Dominik Kutra",
     license="MIT",
     license_files=("LICENSE",),
